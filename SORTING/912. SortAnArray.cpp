d2681c9476b395ae0427010429d693d4c60243c8
Shagun Sharma <119026286+Shagun1611@users.noreply.github.com>
2024-07-10 10:57:51 +0530

    Delete SORTING directory

--- SORTING/912. SortAnArray.cpp
+++
@@ -1,9 +0,0 @@
-// Sorting in the vector
-
-class Solution {
-public:
-    vector<int> sortArray(vector<int>& nums) {
-    sort(nums.begin(),nums.end());   // Begin of the vector and ending of the vector
-    return nums;   // return after sorting
-    }
-};
